cc52443fad9c0ed7d0b1fcc9088a63cf75ec0295
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-08-07 13:22:12 -0700

    print numbers

--- print_numbers.c
+++ print_numbers.c
@@ -1,26 +1,38 @@
 #include "main.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * printint - function to print int
+ * @ap: numbers to be printed
+ * Return: number of character
+ */
+
+int print(va_list ap)
+{
+	int num = va_arg(ap, int);
+	return (_putchar(num));
+}
 
 /**
  * print_numbers - function to print numbers
- * @op: string to be printed among the line
- * @n: number of integers passed to the function
+ * @c:
+ *
  */
-
-int print_int(int n)
+int print_numbers(char c, va_list)
 {
-	int count = 0;
+	unsigned int x = 0;
+	find_format frmmt[] = {
+		{'d', printint},
+		{'i', printint},
+		{'%', print_percent},
+		{'\0', NULL}
+	};
 
-	if (n < 0)
-	{_putchar('-');
-		count = 1;
-		n = -n;
+	for (; frmt[x].spac; x++)
+	{
+		if (c == frmt[x].spac)
+			return (frmt[x].f);
 	}
-
-	if (n / 10)
-		count += print_int(n / 10);
-	_putchar(n % 10 + '0');
-	count +1;
-	return (count);
+	return (NULL);
 }
